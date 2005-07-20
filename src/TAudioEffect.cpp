2a762276bd430097eec45e4d7c5a82e357741d25
letz <letz>
2005-07-20 08:25:01 +0000

    Correct process

--- src/TAudioEffect.cpp
+++ src/TAudioEffect.cpp
@@ -95,7 +95,7 @@ void TAudioEffectList::Process(float* buffer, long framesNum, long channels)
 			// Swap buffers
 			float** tmp = input;
 			input = tmp_output;
-			output = tmp;
+			tmp_output = tmp;
 		}
 		
 		// Interleave...
