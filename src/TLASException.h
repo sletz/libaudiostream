8d59f0e2764976531fd05424afa285d704ecb04a
Stephane Letz <letz@grame.fr>
2013-09-27 12:51:33 +0200

    Add missing file.

---
+++ src/TLASException.h
@@ -0,0 +1,54 @@
+/*
+
+Copyright (C) Grame 2013
+
+This library is free software; you can redistribute it and modify it under
+the terms of the GNU Library General Public License as published by the
+Free Software Foundation version 2 of the License, or any later version.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License
+for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+Grame Research Laboratory, 9, rue du Garet 69001 Lyon - France
+research@grame.fr
+
+*/
+
+#ifndef __TLASException__
+#define __TLASException__
+
+#include <stdexcept>
+
+//-----------------------------
+// Class LasException
+//-----------------------------
+
+class TLASException : public std::runtime_error {
+
+    public:
+
+        TLASException(const std::string& msg) : std::runtime_error(msg)
+        {}
+        TLASException(char* msg) : std::runtime_error(msg)
+        {}
+        TLASException(const char* msg) : std::runtime_error(msg)
+        {}
+
+        std::string Message()
+        {
+            return what();
+        }
+
+        void PrintMessage()
+        {
+            //std::cerr << what();
+        }
+};
+
+#endif
