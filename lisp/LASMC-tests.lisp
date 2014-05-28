5d1b8bc593e1d523fd9fcb9428cd0e8d01f22e1c
Jean Bresson <bresson@ircam.fr>
2014-05-28 16:12:50 +0200

    test modif

--- lisp/LASMC-tests.lisp
+++ lisp/LASMC-tests.lisp
@@ -1,12 +1,13 @@
 (in-package :las)
 
-;(defvar filename1 "/Users/bouche/Desktop/LAS\ 2/audio/30662__erh__tk-4-40.wav")
-;(defvar filename1 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/Bassclarinet1.aif")
-;(defvar filename2 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/bach-4ch.aif")
+;(defparameter filename1 "/Users/bouche/Desktop/LAS\ 2/audio/30662__erh__tk-4-40.wav")
+;(defparameter filename1 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/Bassclarinet1.aif")
+;(defparameter filename2 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/bach-4ch.aif")
+(defparameter filename1 "/Users/letz/Music/Sounds/levot.wav")
+(defparameter filename2 "/Users/letz/Music/Sounds/tango.wav")
 
 
-(defvar filename1 "/Users/letz/Music/Sounds/levot.wav")
-(defvar filename2 "/Users/letz/Music/Sounds/tango.wav")
+
 
 (defvar SR 44100)
 
@@ -17,7 +18,7 @@
                    (fli::dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurFrame) :type :unsigned-long)
                    (fli::dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurUsec) :type :unsigned-long)
                    (/ (fli::dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurUsec) :type :unsigned-long) 1000000.0)))
-    (fli:dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurFrame) :type :unsigned-long)))
+    (fli:dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurFrame) :type :unsigned-long))
 
 (defun getcurdate ()
   (nth 4 (getaudiorendererinfo (getaudioplayerrenderer gaudioplayer))))
@@ -30,12 +31,14 @@
 ;;; (test-time)
 ;;; on voit bien fCurFrame mais fCurUsec toujours à 0
 
+;; (GetLastLibError)
 
 #|
 ;;; SEQUENCE DE TEST
 ;; charge les bindings
 (las::libaudiostream-framework)
-(defvar gAudioPlayer (openaudioplayer 2 2 SR 512 (* 65536 4) (* SR 60 20) kcoreaudiorenderer 1))
+(defvar gAudioPlayer (openaudioplayer 2 2 SR 512 (* 65536 4) (* SR 60 20) kjackrenderer 1))
+(CloseAudioPlayer gAudioPlayer)
 ;;;kcoreaudiorenderer marche, ou kjackrenderer si serveur Jack démarré
 
 ;Démarre le player
@@ -46,7 +49,9 @@
 ;(getdevicecount (getaudioplayerrenderer gaudioplayer)) ;;;renvoie bien 1
 ;(GetDeviceInfo (getaudioplayerrenderer gaudioplayer) 0)
 
-(setq s1 (makeregionsound filename1 500 1105))  
+(probe-file filename1)
+
+(setq s1 (makeregionsound filename1 500 1105))
 
 (setq s1 (makeregionsound filename1 (* SR 2) (* SR 20)))  
 (setq s2 (makeregionsound filename2 (* SR 2) (* SR 20)))  
