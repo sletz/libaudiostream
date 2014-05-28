1692ad47ea1c257476aed28f3c57f865d66ebf1e
Jean Bresson <bresson@ircam.fr>
2014-05-28 17:33:03 +0200

    Lisp bindings & test

--- lisp/LASMC-tests.lisp
+++ lisp/LASMC-tests.lisp
@@ -1,11 +1,20 @@
+
+
+
+(load (merge-pathnames "FFI/load-cffi.lisp" *load-pathname*))
+(load (merge-pathnames "LibAudioStream.lisp" *load-pathname*))
+
+
 (in-package :las)
+
 
 ;(defparameter filename1 "/Users/bouche/Desktop/LAS\ 2/audio/30662__erh__tk-4-40.wav")
 ;(defparameter filename1 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/Bassclarinet1.aif")
-;(defparameter filename2 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/bach-4ch.aif")
+;(defparameter filename2 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/bach-4ch.aiff")
 (defparameter filename1 "/Users/letz/Music/Sounds/levot.wav")
 (defparameter filename2 "/Users/letz/Music/Sounds/tango.wav")
 
+(defvar SR 44100)
 
 ;; Apply a same volume on all channels
 (defun apply-master-volume (sound)
@@ -25,40 +34,55 @@
    (print (list channels name json))
    (MakeEffectSound sound effect 100 100)))
 
-(defvar SR 44100)
 
-(defun getcurdate ()
+(defun print-date ()
   (cffi:with-foreign-object (renderer-info '(:struct rendererinfo))
-     renderer-info)
+    (get-audio-renderer-info (getaudioplayerrenderer gaudioplayer) renderer-info)
     (print (format nil "frame = ~A / usec = ~A / sec = ~A" 
-                   (fli::dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurFrame) :type :unsigned-long)
-                   (fli::dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurUsec) :type :unsigned-long)
-                   (/ (fli::dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurUsec) :type :unsigned-long) 1000000.0)))
-    (fli:dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurFrame) :type :unsigned-long))
+                   (cffi::mem-aref (cffi:foreign-slot-pointer renderer-info '(:struct rendererinfo) 'fCurFrame) :unsigned-long-long)
+                   (cffi::mem-aref (cffi:foreign-slot-pointer renderer-info '(:struct rendererinfo) 'fCurUsec) :unsigned-long-long)
+                   (/ (cffi::mem-aref (cffi:foreign-slot-pointer renderer-info '(:struct rendererinfo) 'fCurUsec) :unsigned-long-long) 1000000.0)))
+   ))
 
-(defun getcurdate ()
+(defun get-date ()
+  (cffi:with-foreign-object (renderer-info '(:struct rendererinfo))
+    (get-audio-renderer-info (getaudioplayerrenderer gaudioplayer) renderer-info) 
+    (cffi::mem-aref (cffi:foreign-slot-pointer renderer-info '(:struct rendererinfo) 'fCurFrame) :unsigned-long-long)))
+
+
+
+(defun get-date-from-info ()
   (nth 4 (getaudiorendererinfo (getaudioplayerrenderer gaudioplayer))))
 
+
+; fInput fOutput fSampleRate fBufferSize fCurFrame fCurUsec fOutputLatencyFrame fOutputLatencyUsec fInputLatencyFrame fInputLatencyUsec)
 (defun test-time ()
-  (loop for i from 0 to 10 do 
+  (loop for i from 0 to 5 do 
         (print (getaudiorendererinfo (getaudioplayerrenderer gaudioplayer)))
         (sleep 1)))
 
-;;; (test-time)
-;;; on voit bien fCurFrame mais fCurUsec toujours à 0
+
 
 ;; (GetLastLibError)
+
+
 
 #|
 ;;; SEQUENCE DE TEST
 ;; charge les bindings
 (las::libaudiostream-framework)
-(defvar gAudioPlayer (openaudioplayer 2 2 SR 512 (* 65536 4) (* SR 60 20) kjackrenderer 1))
-(CloseAudioPlayer gAudioPlayer)
+
+(defvar gAudioPlayer (openaudioplayer 2 2 SR 512 (* 65536 4) (* SR 60 20) kcoreaudiorenderer 1))
+
+;(CloseAudioPlayer gAudioPlayer)
 ;;;kcoreaudiorenderer marche, ou kjackrenderer si serveur Jack démarré
 
 ;Démarre le player
 (startaudioplayer gaudioplayer)
+
+
+; (get-date)
+;;; (test-time)
 
 ;(stopaudioplayer gaudioplayer)
 
@@ -66,19 +90,18 @@
 ;(GetDeviceInfo (getaudioplayerrenderer gaudioplayer) 0)
 
 (probe-file filename1)
+(probe-file filename2)
 
-(setq s1 (makeregionsound filename1 500 1105))
-
-(setq s1 (makeregionsound filename1 (* SR 2) (* SR 20)))  
-(setq s2 (makeregionsound filename2 (* SR 2) (* SR 20)))  
+(setq s1 (makeregionsound filename1 (* SR 0) (* SR 5)))  
+(setq s2 (makeregionsound filename2 (* SR 0) (* SR 2)))  
 
 
 ;;; Problème dans la récupération des arguments côté LAS?
 
-(startsound gaudioplayer (makeregionsound filename1 (* SR 2) (* SR 20)) (genrealdate gaudioplayer 0))  
+(startsound gaudioplayer (makeregionsound filename1 (* SR 2) (* SR 20)) (genrealdate gaudioplayer (get-date)))
 (startsound gaudioplayer (makeregionsound filename2 (* SR 2) (* SR 20)) (genrealdate gaudioplayer 0))  
 
-(startsound gaudioplayer s1 (genrealdate gaudioplayer 0))  
+(startsound gaudioplayer s1 (genrealdate gaudioplayer (getdatefrominfo)))  
 (startsound gaudioplayer s2 (genrealdate gaudioplayer 0))
 
 (resetsound s1)
