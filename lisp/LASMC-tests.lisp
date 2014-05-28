0c20d926343baac46bd7f796538ca4faba14907f
Jean Bresson <bresson@ircam.fr>
2014-05-28 12:04:05 +0200

    LAS test

--- lisp/LASMC-tests.lisp
+++ lisp/LASMC-tests.lisp
@@ -1,8 +1,8 @@
 (in-package :las)
 
 ;(defvar filename1 "/Users/bouche/Desktop/LAS\ 2/audio/30662__erh__tk-4-40.wav")
-(defvar filename1 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/Bassclarinet1.aif")
-(defvar filename2 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/bach-4ch.aif")
+;(defvar filename1 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/Bassclarinet1.aif")
+;(defvar filename2 "/Users/bresson/_SHARED-FILES/IN-FILES/SOUNDFILES/bach-4ch.aif")
 
 (defvar SR 44100)
 
@@ -15,15 +15,6 @@
                    (/ (fli::dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurUsec) :type :unsigned-long) 1000000.0)))
     (fli:dereference (cffi:foreign-slot-pointer renderer-info '(:struct trendererinfo) 'fCurFrame) :type :unsigned-long)))
 
-; (las::libaudiostream-framework)
-; Alloue un Player avec 4 entrées/sorties, une entrée TR d'au plus 10 min, et le backend JACK
-;(defvar gAudioPlayer (openaudioplayer 2 2 SR 512 (* 65536 4) (* SR 60 20) kcoreaudiorenderer 1))
-;;;kcoreaudiorenderer marche, ou kjackrenderer si serveur Jack démarré
-
-;Démarre le player
-;(startaudioplayer gaudioplayer)
-;(stopaudioplayer gaudioplayer)
-
 (defun getcurdate ()
   (nth 4 (getaudiorendererinfo (getaudioplayerrenderer gaudioplayer))))
 
@@ -31,15 +22,30 @@
   (loop for i from 0 to 10 do 
         (print (getaudiorendererinfo (getaudioplayerrenderer gaudioplayer)))
         (sleep 1)))
+
 ;;; (test-time)
 ;;; on voit bien fCurFrame mais fCurUsec toujours à 0
 
 
+#|
+;;; SEQUENCE DE TEST
+;; charge les bindings
+(las::libaudiostream-framework)
+(defvar gAudioPlayer (openaudioplayer 2 2 SR 512 (* 65536 4) (* SR 60 20) kcoreaudiorenderer 1))
+;;;kcoreaudiorenderer marche, ou kjackrenderer si serveur Jack démarré
+
+;Démarre le player
+(startaudioplayer gaudioplayer)
+
+;(stopaudioplayer gaudioplayer)
+
 ;(getdevicecount (getaudioplayerrenderer gaudioplayer)) ;;;renvoie bien 1
 ;(GetDeviceInfo (getaudioplayerrenderer gaudioplayer) 0)
 
-;(setq s1 (makeregionsound filename1 0 1000))   
-;;; Problème dans la récupération des arguments côté LAS
+(setq s1 (makeregionsound filename1 500 1105))  
+;;; Problème dans la récupération des arguments côté LAS?
 
-;(startsound gaudioplayer s1 (genrealdate gaudioplayer (getcurdate)))  ;;;;du coup bus error
+(startsound gaudioplayer s1 (genrealdate gaudioplayer 0))  ;;;; bus error
+
+|#
 
