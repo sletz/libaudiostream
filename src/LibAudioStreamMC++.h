cc2181fa5dc588d8387f5e03fea32ad02d111440
Stephane Letz <letz@grame.fr>
2014-05-28 13:29:47 +0200

    Cleanup LibAudioStreamMC++.h.

--- src/LibAudioStreamMC++.h
+++ src/LibAudioStreamMC++.h
@@ -1,24 +1,24 @@
 /*
-
-Copyright (C) Grame 2002-2013
-
-This library is free software; you can redistribute it and modify it under
-the terms of the GNU Library General Public License as published by the
-Free Software Foundation version 2 of the License, or any later version.
-
-This library is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License
-for more details.
-
-You should have received a copy of the GNU Library General Public License
-along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-Grame Research Laboratory, 9, rue du Garet 69001 Lyon - France
-research@grame.fr
-
-*/
+ 
+ Copyright (C) Grame 2002-2013
+ 
+ This library is free software; you can redistribute it and modify it under
+ the terms of the GNU Library General Public License as published by the
+ Free Software Foundation version 2 of the License, or any later version.
+ 
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License
+ for more details.
+ 
+ You should have received a copy of the GNU Library General Public License
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ 
+ Grame Research Laboratory, 9, rue du Garet 69001 Lyon - France
+ research@grame.fr
+ 
+ */
 
 #ifndef __LibAudioStreamMCPlusPlus__
 #define __LibAudioStreamMCPlusPlus__
@@ -46,25 +46,10 @@ research@grame.fr
 extern "C"
 {
 #endif
-
+    
     typedef int64_t audio_frames_t;
     
     enum {kPortAudioRenderer = 0, kJackRenderer, kCoreAudioRenderer, kOffLineAudioRenderer};
-    
-    /*!
-     \brief Sound channel info
-     */
-    typedef struct ChannelInfo* ChannelInfoPtr;
-    
-    typedef struct ChannelInfo {
-        long fStatus;  // 1 = playing , 0 = idle
-        long fCurFrame;
-        float fVol;
-        float fPanLeft;
-        float fPanRight;
-        long fLeftOut;
-        long fRightOut;
-    } ChannelInfo;
     
     /*!
      \brief Audio device info.
@@ -140,16 +125,16 @@ extern "C"
         }
         
         // Pure virtual : to be implemented by sub-classes
-		
+        
         virtual void Process(float** input, float** output, long framesNum, long channels) = 0;
         virtual TAudioEffectInterface* Copy() = 0;
         virtual void Reset() = 0;
         virtual long Channels() = 0;
-		
-		virtual long GetControlCount() = 0;
-		virtual void GetControlParam(long param, char* label, float* min, float* max, float* init) = 0;
-		virtual void SetControlValue(long param, float f) = 0; 
-		virtual float GetControlValue(long param) = 0;
+        
+        virtual long GetControlCount() = 0;
+        virtual void GetControlParam(long param, char* label, float* min, float* max, float* init) = 0;
+        virtual void SetControlValue(long param, float f) = 0; 
+        virtual float GetControlValue(long param) = 0;
         
     };
     
@@ -383,7 +368,6 @@ extern "C"
      \param sound The stream.
      \param buffer A buffer to be filled with frames.
      \param buffer_size The buffer length.
-     \param channels The number of channels in the buffer.
      \return The number of read frames.
      */
     long ReadSound(AudioStream sound, float** buffer, long buffer_size);
@@ -395,7 +379,6 @@ extern "C"
      \param buffer_size The buffer length.
      \param frames The number of frames to render length.
      \param pos The position in buffer.
-     \param channels The number of channels in the buffer.
      \return The number of read frames.
      */
     long ReadSoundPos(AudioStream sound, float** buffer, long buffer_size, long frames, long pos);
@@ -782,7 +765,7 @@ extern "C"
     /*@}*/
     
     /*! @} */
-
+    
 #ifdef __cplusplus
 }
 #endif
